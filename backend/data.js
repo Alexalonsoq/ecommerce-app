e868f52e48124612893ef8c110ad8ebc287be23f
Alejandro Alonso <Alexalonsoq@users.noreply.github.com>
2021-04-14 18:33:38 -0500

    backend

---
+++ backend/data.js
@@ -0,0 +1,71 @@
+const data = {
+    products: [{
+            _id: '1',
+            name: 'T-shirt dino',
+            category: 'Shirt',
+            image: '/images/product-1.jpg',
+            price: '150',
+            countInStock: 10,
+            brand: 'Puma',
+            rating: '4.5',
+            numReviews: '10',
+            description: 'Playera reptar',
+
+        },
+        {
+            _id: '2',
+            name: 'T-shirt dinosaurio',
+            category: 'Shirt',
+            image: '/images/product-1.jpg',
+            price: '150',
+            countInStock: 0,
+            brand: 'adidas',
+            rating: '5',
+            numReviews: '0',
+            description: 'Playera reptar',
+
+        },
+        {
+            _id: '3',
+            name: 'T-shirt dino',
+            category: 'Shirt',
+            image: '/images/product-1.jpg',
+            price: '50',
+            countInStock: 4,
+            brand: 'Puma',
+            rating: '4.5',
+            numReviews: '0',
+            description: 'Playera reptar',
+
+        },
+        {
+            _id: '4',
+            name: 'T-shirt dinosaurio',
+            category: 'Shirt',
+            image: '/images/product-1.jpg',
+            price: '10',
+            countInStock: 100,
+            brand: 'Puma',
+            rating: '3.5',
+            numReviews: '0',
+            description: 'Playera reptar',
+
+        },
+        {
+            _id: '5',
+            name: 'T-shirt dino mayor',
+            category: 'Shirt',
+            image: '/images/p2.jpg',
+            price: '250',
+            countInStock: 30,
+            brand: 'Puma',
+            rating: '4.5',
+            numReviews: '0',
+            description: 'Playera reptar',
+
+        },
+
+    ],
+};
+
+export default data;
